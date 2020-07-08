18ba3447995c3f86a260107082a9bea5064c6764
yavuzalikorkut <yavuzalikorkut@outlook.com>
2020-07-08 18:32:54 +0300

    Fix bug

--- modules/one_sample/sign_test/views.py
+++ modules/one_sample/sign_test/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, session
 from utils import helper
 from scipy import stats
 import pandas as pd
+import numpy as np
 
 
 blueprint = Blueprint('signtest', __name__)
@@ -21,8 +22,12 @@ def fileUpload():
 @blueprint.route('/one-sample/sign-test/result', methods=('POST',))
 def signtestResult():
     variable = request.form.getlist('varcheckbox[]')
+    popmean = request.form.get('popmeansigntest')
+    checkval = pd.to_numeric(popmean)
     file = session["FilePath"]
     df = pd.read_csv("static/uploads/{}".format(file))
     selectedvars = df[variable].values
-    t, p = stats.wilcoxon(selectedvars[0])
+    ini_array1 = np.array(selectedvars)
+    onedimensional = ini_array1.flatten()
+    t, p = stats.wilcoxon(onedimensional-checkval)
     return render_template('one-sample-tests/sign-test/sign-test-result.html', t=t, p=p)
